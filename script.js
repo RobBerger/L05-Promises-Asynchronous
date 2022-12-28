9a197dc4a1cff4dd1c44ff2426077ebce923e468
berger <robertberger.dev@gmail.com>
2022-12-28 17:22:05 -0500

    await fetch dog example

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let conditionToBeMet = true;
+// let conditionToBeMet = true;
 
-async function myPromise() {
-    const result = await new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            if (conditionToBeMet) {
-                resolve('promise resolved');
-            } else {
-                reject("promise rejected");
-            }
-        }, 3000);
-    });
-    console.log(result);
-    return result;
-};
+// async function myPromise() {
+//     const result = await new Promise(function (resolve, reject) {
+//         setTimeout(function () {
+//             if (conditionToBeMet) {
+//                 resolve('promise resolved');
+//             } else {
+//                 reject("promise rejected");
+//             }
+//         }, 3000);
+//     });
+//     console.log(result);
+//     return result;
+// };
 
-myPromise()
-    .then(res => console.log('Success: ' + res))
-    .catch(err => console.log('Error: ' + err));
+// myPromise()
+//     .then(res => console.log('Success: ' + res))
+//     .catch(err => console.log('Error: ' + err));
+
+async function fetchDog() {
+    let myDog = await fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json());
+
+    console.log('myDog result: ', myDog);
+    console.log('status: ', myDog.status);
+
+    var imgtag = document.createElement('img');
+    imgtag.setAttribute('src', myDog.message);
+    document.body.appendChild(imgtag);
+}
+
+fetchDog();
