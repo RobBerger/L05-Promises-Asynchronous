a6fd43dfef107cdd05ef973a958e30d0e0f9efb5
berger <robertberger.dev@gmail.com>
2022-12-28 13:20:26 -0500

    setTimeout used in a new variable

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 new Promise(function (resolve, reject) {
     //   ....INSERT LONG-RUNNING OPERATION HERE...  
 });
+
+const myPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('promise resolved');
+    }, 3000);
+})
